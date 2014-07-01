3c3cd64c32a2c328ae2b03eb526a5bb26485d208
ALKA-Productions <alkagameproductions@gmail.com>
2014-07-01 10:41:09 +1000

    Index Update

--- Resources/MyJS.js
+++ Resources/MyJS.js
@@ -1,25 +1,48 @@
 $(document).ready(function(){
 
-		showImg($("#Image1"));
 
 	$("#th1").click(function(){
-		
 		showImg($("#Image1"));
 		
 	});
 
 	$("#th2").click(function(){
-		
 		showImg($("#Image2"));
 	});
+
+	$("#th3").click(function(){
+		showImg($("#Image3"));
+	});
+
+	slideShow();
 
 });
 
 function showImg(elem)
 {
-	if (elem.css("z-index") == 900) return;
-		$("#ImageScroller>img").css("z-index", 100);
-		elem.hide();
-		elem.css("z-index", 900);
-		elem.fadeIn();
+	if (elem.hasClass("current")) 
+		return;
+
+	$(".previous").removeClass("previous");
+	$(".current").addClass("previous");
+	$(".current").removeClass("current");
+	elem.hide();
+	elem.fadeIn();
+	elem.addClass("current");
+}
+
+function slideShow(){
+
+	var I1 = true;
+	var I2 = false;
+	var I3 = false;
+
+
+	setInterval(function(){
+		if ($("#ImageScroller>img.current").next("#ImageScroller>img").length == 0) {
+			showImg($("#ImageScroller>img:nth-child(1)"));
+			return;	
+		}
+		showImg($("#ImageScroller>img.current").next("#ImageScroller>img"));
+	}, 5000);
 }
